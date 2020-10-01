1fa0ca67ce97cc382ab9afc352996e3739e7a669
elgringo32 <bknopke@gmail.com>
2020-09-30 22:55:32 -0700

    added mouseover event listener

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 var container = document.getElementById('container');
+
 for (i=0; i < 16; i++) {
     let div = document.createElement('div')
+    div.classList.add('square')
     container.appendChild(div);
 }
+
+function makeActive(e) {
+    e.target.classList.add('activated');
+}
+
+container.addEventListener('mouseover', makeActive);    
+
+
