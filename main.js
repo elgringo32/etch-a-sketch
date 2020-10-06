436deb879cadc6dc39e27e794435880a76123004
elgringo32 <bknopke@gmail.com>
2020-10-06 06:59:32 -0700

    added clear btn that resets the drawing board

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
 var container = document.getElementById('container');
+buildGrid(prompt("Enter Grid Size"));
 
-for (i=0; i < 16; i++) {
-    let div = document.createElement('div')
-    div.classList.add('square')
-    container.appendChild(div);
+
+function buildGrid (gridSize) {
+    var squareSize = (960)/gridSize;
+    console.log(squareSize);
+    console.log(gridSize);
+    for (i=0; i < gridSize*gridSize; i++) {
+        let addDiv = document.createElement('div');
+        addDiv.classList.add('square');
+        addDiv.style.height = `${squareSize}px`;
+        addDiv.style.width = `${squareSize}px`;
+        container.appendChild(addDiv);
+
+    }
+
+    var divs = document.querySelectorAll('.square');
+
+    divs.forEach(div => div.addEventListener('mouseover', makeActive)
+);    
+}
+
+function removeActive() {
+    var divs = document.querySelectorAll('.square');
+    divs.forEach(div => container.removeChild(div));
+    buildGrid(prompt("Enter Grid Size"));
+    
 }
 
 function makeActive(e) {
     e.target.classList.add('activated');
 }
 
-container.addEventListener('mouseover', makeActive);    
+
+var btn = document.querySelector('button');
+btn.addEventListener('click', removeActive);
 
 
